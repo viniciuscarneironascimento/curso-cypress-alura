694e682975bbb42150bee5032677ccc20cca2fa7
Seu Nome <seuemail@exemplo.com>
2025-02-19 11:38:52 -0300

    Limpeza do cache

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,23 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-         setupNodeEvents(on, config) {
+    setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     video: true,
-      reporter: 'mochawesome',
-      reporterOptions: {
-        reportDir: 'cypress/results',
-        overwrite: false,
-        html: true,
-        json: false,
-        timestamp: "mmddyyyy_HHMMss"
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/results',
+      overwrite: false,
+      html: true,
+      json: false,
+      timestamp: "mmddyyyy_HHMMss"
     },
-   
-  }
+
+  },
+
 });
